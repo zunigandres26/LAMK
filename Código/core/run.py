c334c475850ae455cc408a843bfadfe2281749d8
Lxr_Dev <alexis_danielGT@hotmail.com>
2020-08-18 20:22:31 -0600

    Documentación Interna

--- Código/core/run.py
+++ Código/core/run.py
@@ -15,12 +15,22 @@ from Reader import Reader
 from SyntaxAnalyzer import SyntaxAnalyzer
 from tabulate import tabulate
 
-
+"""
+    La clase execute es la encargada de filtrar los parámetros y ejecutar las funciones correspondientes.
+"""
 class execute:
 
+    """
+        Constructor
+        @param parameters: Arreglo con los parámetros obtenidos desde consola.
+    """
     def __init__(self, parameters):
         self.parameters = parameters
 
+    """
+        La función run es la encargada de verificar la integridad de los parámetros y tomar una decisión
+        de acuerdo a los parámetros enviados.
+    """
     def run(self):
         if len(self.parameters) > 1:
             if len(self.parameters) == 3:                
@@ -31,7 +41,7 @@ class execute:
                 elif self.parameters[1] == "--recognize":
                     self.recognize(self.parameters[2])  
                 else:
-                    print("Comando no soportado o desconocido")
+                    print("Comando no soportado o desconocido.")
                     self.help()         
             elif len(self.parameters) == 2:
                 if self.parameters[1] == "--info":
@@ -39,14 +49,19 @@ class execute:
                 elif self.parameters[1] == "--help":
                     self.help()
                 else: 
-                    print("Comando no soportado o desconocido")
+                    print("Comando no soportado o desconocido.")
                     self.help()
             else:
-                print("Comando no soportado o desconocido")
+                print("Comando no soportado o desconocido.")
                 self.help()
         else:
             self.help()
     
+    """
+        La función exec lee y analiza el código.
+        Pasa por el analizador léxico, sintáctico y semántico.
+        @param filename: Nombre del archivo a leer.
+    """
     def exec(self, filename):
         # Ejecución del analizador Léxico
         reader = (Reader()).reader(filename)
@@ -65,13 +80,27 @@ class execute:
         except Exception as e:
             print ("Error: %s" % e)
 
-
+    """
+        La función recognize ejecuta al Analizador sintáctico, el cual se encarga de reconocer
+        los lenguajes soportados (Javascript, Ruby, Bash).
+        @param filename: Nombre del archivo a leer.
+    """
     def recognize(self, filename):
         reader = (Reader()).reader(filename)
         sintactic = (SyntaxAnalyzer(reader).run())
         print("Se ha reconocido al Lenguaje %s" % sintactic.getLanguage( sintactic.language ))
         print("\n\nEl código leído es:\n",sintactic.getCode(sintactic.statements))
+
+    def blockPrint(self):
+        sys.stdout = open(os.devnull, 'w')
+
+    def enablePrint(self):
+        sys.stdout = sys.__stdout__
     
+    """
+        La función tabview muestra la tabla de símbolos.
+        @param filename: Nombre del archivo a leer.
+    """
     def tabview(self, filename):
         reader = (Reader()).reader(filename)
         sintactic = (SyntaxAnalyzer(reader).run())
@@ -93,6 +122,9 @@ class execute:
         self.enablePrint()
         print(tabulate(result, headers=["#","Lexema","Initial Value","Token","Final Value"], showindex="always", tablefmt="fancy_grid"))
 
+    """
+        La función help muestra la ayuda al usuario con los comandos soportados.
+    """
     def help(self):
         print("Bienvenido a la utilidad de ayuda de LIR 1.0\n")
         print("Ejecutar el programa                  --exec fileName")
@@ -101,6 +133,9 @@ class execute:
         print("Muestra la informacion del programa   --info")
         print("Comandos soportados                   --help")
     
+    """
+        La función info muestra la información de los autores y la versión.
+    """
     def info(self):
         print("*"*45)
         print("* Interprete LRI", " "*26, "*\n*"," "*41, "*")
@@ -112,8 +147,4 @@ class execute:
         print("* @date 2020-08-17", " "*24, "*")
         print("*"*45)
 
-    def blockPrint(self):
-        sys.stdout = open(os.devnull, 'w')
-
-    def enablePrint(self):
-        sys.stdout = sys.__stdout__
+    
